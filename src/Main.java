b55a6a8132fdf4a9dbad4626149e2ec84af21a65
Кошелев Александр <Lazurus_777@mail.ru>
2025-11-25 21:14:01 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,18 @@
 
+public class Main {
+    public static void main(String[] args) {
+
+        byte byteVar = 127;
+        short shortVar = 32767;
+        int intVar = 2147483647;
+        long longVar = 9223372036854775807L;
+        float floatVar = 3.14f;
+        double doubleVar = 3.1415926535;
+        char charVar = 'A';
+        boolean booleanVar = true;
+    }
+
+    public static void greet(String name) {
+        System.out.println("Привет, " + name + "!");
+    }
+}
